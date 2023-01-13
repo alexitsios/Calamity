f4d6694dc94a5e5806417b15eeccfab85f904a99
vitormaduro <vitormaduro006@gmail.com>
2023-01-13 17:49:58 -0300

    Basis for Ink integration

---
+++ CalamityFunigami/Assets/Script/Ink Implementation/ScriptDialogManager.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDialogManager : MonoBehaviour
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="message"></param>
+	public void DisplayDialog(string message, List<string> tags)
+	{
+
+	}
+}

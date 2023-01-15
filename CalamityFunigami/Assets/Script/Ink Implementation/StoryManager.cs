f7a4b6844bbd3fbc37782d0a134260b9b7df7ff5
vitormaduro <vitormaduro006@gmail.com>
2023-01-15 20:09:33 -0300

    New test scene with interactive objects and working movement

--- CalamityFunigami/Assets/Script/Ink Implementation/StoryManager.cs
+++ CalamityFunigami/Assets/Script/Ink Implementation/StoryManager.cs
@@ -1,17 +1,31 @@
 using Ink.Runtime;
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScriptManager : MonoBehaviour
+public class StoryManager : MonoBehaviour
 {
 	#region Public Variables
-	public static ScriptManager Instance { get; private set; }
+	public static StoryManager Instance { get; private set; }
+	#endregion
+
+	#region Editor Variables
+	[SerializeField]
+	[Obsolete("This field will be removed in the future. The script of the scene should be loaded automatically")]
+	private TextAsset script;
+
+	[SerializeField]
+	[Tooltip("The Prefab of the button used to display choices during dialog")]
+	private GameObject choiceButton;
 	#endregion
 
 	#region Private Variables
-	private TextAsset script;
 	private Story inkStory;
 	private ScriptCommandsManager commandsManager;
 	private ScriptDialogManager dialogManager;
+	private List<GameObject> activeChoiceButtons = new();
 	#endregion
 
 	private void Awake()
@@ -41,6 +55,12 @@ public class ScriptManager : MonoBehaviour
 	/// <param name="knotName">The knot's name</param>
 	public void JumpToKnot(string knotName)
 	{
+		// TODO: improve this. The story should load only once, when the scene loads
+		if(inkStory == null)
+		{
+			LoadInkStory();
+		}
+
 		inkStory.ChoosePathString(knotName);
 	}
 
@@ -54,6 +74,11 @@ public class ScriptManager : MonoBehaviour
 			var currentLine = inkStory.Continue();
 			var tags = inkStory.currentTags;
 
+			if(string.IsNullOrWhiteSpace(currentLine))
+			{
+				continue;
+			}
+
 			if(currentLine.StartsWith(">>"))
 			{
 				commandsManager.RunCommand(currentLine);
@@ -62,6 +87,36 @@ public class ScriptManager : MonoBehaviour
 			{
 				dialogManager.DisplayDialog(currentLine, tags);
 			}
+
 		}
+
+		if(inkStory.currentChoices.Count > 0)
+		{
+			for(var i = 0; i < inkStory.currentChoices.Count; i++)
+			{
+				var choice = inkStory.currentChoices[i];
+				var parent = GameObject.FindGameObjectWithTag("ButtonParent");
+				var button = Instantiate(choiceButton, parent.transform);
+
+				button.GetComponentInChildren<TMP_Text>().text = choice.text;
+				button.GetComponent<Button>().onClick.AddListener(() => SelectChoice(choice.index));
+				activeChoiceButtons.Add(button);
+			}
+		}
+	}
+
+	/// <summary>
+	///		Deletes all of the option buttons, sets the choice in the script, then continue the story
+	/// </summary>
+	/// <param name="index">The 0-based index of the selected choice</param>
+	private void SelectChoice(int index)
+	{
+		foreach(var button in activeChoiceButtons)
+		{
+			Destroy(button);
+		}
+
+		inkStory.ChooseChoiceIndex(index);
+		ProcessStory();
 	}
 }

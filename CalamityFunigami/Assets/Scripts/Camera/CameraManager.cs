878647d767582ea9da6693cecec8fa0b3cfc6b05
Jordan <jordan.wilgus@gmail.com>
2023-03-17 16:07:30 -0400

    Fixed merge conflicts

--- CalamityFunigami/Assets/Scripts/Camera/CameraManager.cs
+++ CalamityFunigami/Assets/Scripts/Camera/CameraManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public Camera ActiveCamera { get; set; }
+    public AudioListener ActiveListener { get; set; }
 
     private void Start()
     {
         ActiveCamera = Camera.main;
+        ActiveListener = ActiveCamera.GetComponent<AudioListener>();
     }
 }
